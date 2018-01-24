9b9c955cf565d386483ddf4193914e1c5d67e154
ooftf <994749769@qq.com>
2018-01-25 00:31:08 +0800

    修改项目结构

--- app/src/main/java/com/ooftf/support/sample/MainActivity.kt
+++ app/src/main/java/com/ooftf/support/sample/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ooftf.sample
+package com.ooftf.support.sample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.ooftf.support.R
 
 class MainActivity : AppCompatActivity() {
 
